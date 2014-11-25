c43dd6a4d4ff31e23b05b6559415ddb13b14eeee
Yusuke Yachi <yusuke.yachi@gmail.com>
2014-11-25 09:58:53 +0900

    split command was supported

---
+++ lib/casteml/commands/split_command.rb
@@ -0,0 +1,42 @@
+require 'casteml/command'
+require 'casteml/formats/xml_format'
+class Casteml::Commands::SplitCommand < Casteml::Command
+	def initialize
+		super 'split', 'Split single pml-file'
+
+		add_option('-o', '--outfile OUTPUTFILE',
+						'Specify output filename') do |v, options|
+			options[:outfile] = v
+		end
+
+	end
+
+	def usage
+		"#{program_name} PMLFILE"
+	end
+	def arguments
+		"PMLFILE\t pmlfile to be splited (ex; session-all.pml)"
+	end
+
+	def description
+		<<-EOF
+The split command allows you to split single pmlfile and create multiple pmlfiles.
+
+Examples:
+	$ casteml split session-all.pml
+	$ ls
+	session-all.pml session-1.pml session-2.pml session-3.pml
+EOF
+	end
+
+
+	def execute
+		original_options = options.clone
+		args = options.delete(:args)
+		raise OptionParser::InvalidArgument.new('specify PMLFILE') if args.empty?
+
+    	pml_path = args.shift
+    	Casteml::Formats::XmlFormat.split_file(pml_path)
+
+	end
+end
---
+++ spec/casteml/commands/split_command_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'casteml/commands/split_command'
+module Casteml::Commands
+	describe SplitCommand do
+		before(:each) do
+			setup_empty_dir('tmp')
+		end
+		describe "#invoke_with_build_args" do
+			let(:cmd){ SplitCommand.new }
+			let(:build_args){ [] }
+			context "without args" do
+				let(:args){ [] }
+				it "shows error message" do
+					expect(cmd).to receive(:say).with("ERROR: invalid argument: specify PMLFILE. See 'casteml split --help'.")
+					cmd.invoke_with_build_args args, build_args
+				end
+			end
+
+			context "with -h" do
+				let(:args){ ['-h'] }
+				it "shows help" do
+					expect(cmd).to receive(:show_help)
+					cmd.invoke_with_build_args args, build_args
+				end
+			end
+
+			context "with path" do
+				let(:paths){ ['path1',  'path2'] }
+				let(:path){ 'tmp/my-great.pml'}
+				let(:args){ [path]}
+				it "calls split_file with path" do
+					expect(Casteml::Formats::XmlFormat).to receive(:split_file).with(path).and_return(paths)
+					cmd.invoke_with_build_args args, build_args
+				end
+
+			end
+
+		end		
+	end
+end
