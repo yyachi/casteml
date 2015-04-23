3537742e1e68b0b79a9bdf793ecc4b0403ca1b9a
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2015-04-24 00:05:37 +0900

    Revise help for `casteml join' by TK

--- lib/casteml/commands/join_command.rb
+++ lib/casteml/commands/join_command.rb
@@ -2,31 +2,35 @@ require 'casteml/command'
 require 'casteml/formats/xml_format'
 class Casteml::Commands::JoinCommand < Casteml::Command
 	def initialize
-		super 'join', 'Merge multiple pmlfiles to a single pmlfile'
+		super 'join', 'Join several pmlfiles to a single pmlfile'
 
 		add_option('-o', '--outfile OUTPUTFILE',
 						'Specify output filename') do |v, options|
 			options[:outfile] = v
 		end
-
 	end
 
 	def usage
 		"#{program_name} file0 file1 [file2 ...] > outfile"
 	end
 	def arguments
-		"PMLFILES\t pmlfiles to be joined (ex; session-1.pml session-2.pml ... session-n.pml)"
+		"pmlfiles to be joined (ex. session1.pml session2.pml ... sessionN.pml)"
 	end
 
 	def description
-		<<-EOF
+    <<-EOF
+    Join several pmlfiles to a single pmlfile.  Certain command such
+    for `casteml plot' can only accept a single pmlfile (including
+    many datesets).  Use this program to merge the pmlfiles.
+
 EXAMPLE
 	$ casteml join JB3-1.pml stone-1.pml stone-2.pml JB3-2.pml > session.pml
 	$ casteml join JB3-1.pml stone-1.pml stone-2.pml JB3-2.pml -o session.pml
 
 SEE ALSO
+    casteml split
+    casteml download
     http://dream.misasa.okayama-u.ac.jp
-    split
 
 IMPLEMENTATION
     Copyright (c) 2015 ISEI, Okayama University
